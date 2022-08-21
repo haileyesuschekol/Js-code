55db9fa749f27d8082a0c6ec61d98cbdd7f554b3
haileyesus <teamdevs7@gmail.com>
2022-08-21 10:26:35 +0100

    added array methods

--- array.js
+++ array.js
@@ -15,7 +15,28 @@ const yearOfCentury = year =>{
     return calculateYear;
 }
 const yearToCalcualte = [1991, 2011, 2022];
-
 const finalOut =[1990 , 2000, yearOfCentury(yearToCalcualte[1])];
-
 console.log(finalOut);
+
+
+//array methods(operations)
+
+const personOnEvent = ['john','haile','tomas','fayzel','dani'];
+console.log(personOnEvent);
+
+personOnEvent.push('mudi'); // add at the end
+console.log(personOnEvent);
+
+personOnEvent.unshift('nati'); // add at the begninng
+console.log(personOnEvent); 
+
+personOnEvent.pop(); // remove at the end
+console.log(personOnEvent); 
+
+personOnEvent.shift(); // remove at the begninng
+console.log(personOnEvent); 
+
+if(personOnEvent.includes('fayzel')){
+    console.log('welcome'); 
+}else{console.log('no enter'); }
+
