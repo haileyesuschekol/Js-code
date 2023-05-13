ffa1c042bc2d99ea69aa625c0d7f39d8f4fe8b32
haileyesus <teamdevs7@gmail.com>
2023-05-13 18:31:49 +0100

    adding a new js file

---
+++ basics of js/script.js
@@ -0,0 +1,45 @@
+// myname = 'haile'
+// console.log(haile)
+
+
+//operators
+
+
+// const car = 'black'
+// console.log(car)
+
+// + ,- , * , / , %
+//food tip calculator
+
+// const totalFood = Number(prompt('total amount of food'))
+// const tip = Number(prompt(('tip')))
+// const amountTip = tip/100
+// const total = totalFood+tip
+// console.log(total) 
+
+// datatypes
+
+//number
+//string
+//boolean
+//array
+//object
+
+//math methods
+
+//math.floor(3.4)
+//math.ceil(3.8)
+//math.random()
+
+
+
+
+
+
+
+
+
+
+
+
+
