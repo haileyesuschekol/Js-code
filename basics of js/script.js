15df13957d28b67e70096db8a8d91be36bd8d8eb
haileyesus <teamdevs7@gmail.com>
2023-05-19 18:22:49 +0100

    add codes

--- basics of js/script.js
+++ basics of js/script.js
@@ -1,9 +1,9 @@
-// myname = 'haile'
+myname = 'haile'
 // console.log(haile)
 
 //operators
 
-// const car = 'black'
+const car = 'black'
 // console.log(car)
 
 // + ,- , * , / , %
@@ -32,13 +32,13 @@
 
 //conditional statment
 
-// let weather = 'sunny'
+let weather = 'sunny'
 
-// if(weather == 'sunny'){
-//     console.log('wear your sunglasses')
-// }else{
-//     console.log('grab your umbrella')
-// }
+if(weather == 'sunny'){
+    //console.log('wear your sunglasses')
+}else{
+   // console.log('grab your umbrella')
+}
 
 //conditional operators
 // == , === , <,> ,<=,>= ,!== , !=
@@ -46,42 +46,42 @@
 
 //Function
 
-// function sayMyName(){
-// console.log('haile')
-// }
-// sayMyName()
+function sayMyName(){
+console.log('haile')
+}
+//sayMyName()
 
 // function with argument
 
-// function sayMyName2(myName){
-// console.log(myName)
-// }
-// sayMyName2('my name is haile')
+function sayMyName2(myName){
+console.log(myName)
+}
+//sayMyName2('my name is haile')
 
 
 // templet litrals
 
-// function greeting(yourName){
-//     console.log(`hello ${yourName} how are you ?`)
-// }
+function greeting(yourName){
+    console.log(`hello ${yourName} how are you ?`)
+}
 
 // greeting('baba')
 
 
 // return make the function reusable
-// function sum(a, b){
-//     return a+b
-// }
+function sum(a, b){
+    return a+b
+}
 
 
-// function totalFoodCalculator(a,b){
-//      totalFood = a
-//      tip = b
-//      amountTip = b/100*a
-//     const total = sum(a,amountTip)
-//     console.log(total) 
+function totalFoodCalculator(a,b){
+     totalFood = a
+     tip = b
+     amountTip = b/100*a
+    const total = sum(a,amountTip)
+    console.log(total) 
 
-// }
+}
 
 // totalFoodCalculator(400,20)
 
@@ -89,26 +89,26 @@
 //ES6 
 // arrow function with explicite return
 
-// const sumArrow = (a, b) => {
-//     return a+b;
-// }
+const sumArrow = (a, b) => {
+    return a+b;
+}
 // console.log(sumArrow(20,80))
 
 // arrow function with implicit return
 
-// const sumArrow2 = ( a,b) => a+b
+const sumArrow2 = ( a,b) => a+b
 // console.log(sumArrow2(25,25))
 
 // Array
 
-//const items=['apple' , 'banana' , 'mango']
+const items=['apple' , 'banana' , 'mango']
 // console.log(items)
 // console.log(items[2])
 
 //Array methods
 
 //push to add item on the array
-//items.push('orange', 'pinapple','avocado')
+items.push('orange', 'pinapple','avocado')
 //console.log(items)
 
 //slice 
@@ -124,10 +124,10 @@
 //object
 // assigning an object 
 
-// const person = {
-//     Name : 'boss',
-//     age:'25',
-// }
+const person = {
+    Name : 'boss',
+    age:'25',
+}
 
 //accesing an object by dot 
 // console.log(person.Name)
@@ -137,50 +137,96 @@
 // console.log(person['Name'])
 // console.log(person['age'])
 
-// person.phoneNumeber='1-234-567-8910'
+ person.phoneNumeber='1-234-567-8910'
 
 // console.log(person.phoneNumeber)
 
 // examples
 
-// const cars = (color,plateNo) =>{
-//     const car= {
-//         color:color,
-//         plateNo:plateNo,
-//         cost:500000,
-//         tax:function(){
-//             const taxes  = this.cost*0.15
-//             return taxes
-//         }
-//     }
+const cars = (color,plateNo) =>{
+    const car= {
+        color:color,
+        plateNo:plateNo,
+        cost:500000,
+        tax:function(){
+            const taxes  = this.cost*0.15
+            return taxes
+        }
+    }
 
-//     const description =`my car color is ${car.color} and my palte number is ${car.plateNo} and costs ${car.cost} with ${car.tax()} of tax`
-//     return description
-// }
+    const description =`my car color is ${car.color} and my palte number is ${car.plateNo} and costs ${car.cost} with ${car.tax()} of tax`
+    return description
+}
 
 // console.log(cars('black', '12345'))
 
 // for loop
 
-// const friuts =['banana' , 'apple' ,'orange', 'pineapple','banana' , 'apple' ,'orange', 'pineapple']
+const friuts =['banana' , 'apple' ,'orange', 'pineapple','banana' , 'apple' ,'orange', 'pineapple']
 
-// for(let i = 0; i<friuts.length ; i++){
-//     console.log(i+1, friuts[i])
-// }
+for(let i = 0; i<friuts.length ; i++){
+    //console.log(i+1, friuts[i])
+}
 
 // the new pythonic array
 
-// for(fruit of friuts){
-//     console.log(fruit)
-// }
+for(fruit in friuts){
+    //console.log(fruit)
+}
 
 // example 
 
-// const doubel = (arrNumber) =>{
-//     let result=[]
-//     for(const num of arrNumber){
-//         result.push(num*2)
-//     }
-//     return result
-// }
+const doubel = (arrNumber) =>{
+    let result=[]
+    for(const num of arrNumber){
+        result.push(num*2)
+    }
+    return result
+}
 // console.log(doubel([1,2,3,4,5,6]))
+
+//excercise 
+
+const letterCounter = () =>{
+    const letter ='hellooo'
+    for(let letters in letter){
+        letters++
+        console.log(letters)
+    }  
+}
+    //letterCounter()
+
+const addNumbers =(addingNumbers)=>{
+      let results = 0
+    for(const sum of addingNumbers){
+         results +=sum
+    }
+    return results
+}
+
+//console.log(addNumbers([1,2,3,4,5,6,7,8]))
+
+
+const greatestNum =(num)=>{
+    let result = num[0]
+   for(let i=1;i<num.length;i++){
+    if(num[i]>result)
+    result=num[i]
+   }
+   return result
+}
+
+//console.log(greatestNum([2,4,6,4,9,12,3,5,23,6,345,77,234,]))
+
+const maxNum =(num)=>{
+    let result = num[0]
+    for(number of num){
+        if(number>result){
+            result=number
+        }
+    }
+    return {result}
+}
+
+//console.log(maxNum([4,5,6,3,7,5,23,4]))
+
