4f79ddfa44cc99d91a3a94fc750e649db989c212
haileyesus <teamdevs7@gmail.com>
2023-05-24 09:31:21 +0100

    added some excercise

--- DOM/script.js
+++ DOM/script.js
@@ -8,26 +8,50 @@ document.getElementById('title').style.color ='red'
 let green = document.getElementById('green')
 let yellow = document.getElementById('yellow')
 let red = document.getElementById('red')
-let onclick = document.getElementById('onclick')
-
-green.onclick = () => {
-    console.log('green')
-    onclick.innerText='You clicked Green'
-    onclick.style.color='green'
-
-}
-yellow.onclick = () => {
-    console.log('yellow')
-    onclick.innerText='You clicked Yellow'
-    onclick.style.color='yellow'
-
-}
-red.onclick = () => {
-    console.log('red')
-    onclick.innerText='You clicked Red'
-    onclick.style.color='red'
-
-}
-
+let clicker = document.getElementById('click')
 let squares = document.querySelectorAll('.squares')
 
+// squares.onclick = () => {
+//     clicker.innerText='You clicked Yellow'
+//     clicker.style.color='green'
+// }
+// squares.onclick = () => {
+//     clicker.innerText='You clicked Yellow'
+//     clicker.style.color='yellow'
+
+// }
+// squares.onclick = () => {
+//     clicker.innerText='You clicked Red'
+//     clicker.style.color='red'
+
+// }
+
+
+// console.log(squares[0].value)
+// console.log(squares[1].value)
+// console.log(squares[2].value)
+
+const timeClicked={
+    'red':0, 'yellow':0 ,'green':0
+}
+
+//for each
+
+squares.forEach(squares=>{
+    squares.onclick = () => {
+        //console.log(squares.value)
+        timeClicked[squares.value] += 1
+        squares.innerHTML = timeClicked[squares.value]        
+    }
+})
+
+const cleargame = document.getElementById('clearbtn')
+
+let clearbutton = () =>{
+    squares.forEach(squares => {
+        squares.innerText= ' '
+        timeClicked[squares.value] = 0
+} )
+}
+
+cleargame.onclick =() => clearbutton()
