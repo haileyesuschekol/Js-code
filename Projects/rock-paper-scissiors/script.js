76269e55ba4261fa8c972d214ed9cc9a254674e3
haileyesus <teamdevs7@gmail.com>
2023-05-28 11:18:18 +0100

    added a project file

---
+++ Projects/rock-paper-scissiors/script.js
@@ -0,0 +1,35 @@
+
+// ** getComputerChoice randomly selects between `rock` `paper` `scissors` and returns that string **
+function getComputerChoice() {
+  
+}
+
+// ** getResult compares playerChoice & computerChoice and returns the score accordingly **
+
+function getResult(playerChoice, computerChoice) {
+ 
+  
+}
+
+// ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
+function showResult(score, playerChoice, computerChoice) {
+
+}
+
+// ** Calculate who won and show it on the screen **
+function onClickRPS(playerChoice) {
+  
+}
+
+
+// ** Make the RPS buttons actively listen for a click and do something once a click is detected **
+function playGame() {
+  
+}
+
+// ** endGame function clears all the text on the DOM **
+function endGame() {
+  
+}
+
+playGame()
