1dfe254bee3737c1ca9c1c624be4f6e4effaf43d
haileyesus <teamdevs7@gmail.com>
2023-05-30 18:50:45 +0100

    display name and status to the hero

--- Projects/get-random-hero/script.js
+++ Projects/get-random-hero/script.js
@@ -5,21 +5,42 @@ const displayImg = document.getElementById('display')
 const getNewHero = document.getElementById('getNewHero')
 const inputField = document.getElementById('inputField')
 const searchBtn = document.getElementById('searchBtn')
+const nameOfHero =document.getElementById('nameOfHero')
+const statusOfHero = document.getElementById('statusOfHero')
 
 const getHero = (id) =>{
     fetch(`${BaseUrl}/${id}`)
     .then(response => response.json())
     .then(json => {
-        console.log(json)
+        console.log(json.powerstats)
+        statusOfHero.innerHTML = `<h3> Intelligence = ${json.powerstats['intelligence']}  </h3>`
+        nameOfHero.innerText= json.name
         let heroImg = json.image.url
         displayImg.innerHTML =`<img src = "${json.image.url}" height = 250 width =200/>`
 
     })
 }
 
+const searchSuperHero =(name)=>{
+    //console.log(inputField.value)
+    fetch(`${BaseUrl}/search/${name}`)
+    .then(response=>response.json())
+ .then(json=>{
+    const hero =json.results[0]
+    //nameOfHero.innerText=`<h2>${json.name}</h2>`
+        displayImg.innerHTML =`<img src = "${hero.image.url}" height = 250 width =200/>`
 
+})
+}  
+
+searchBtn.onclick=()=>searchSuperHero(inputField.value)
 //console.log(randomId)
 getNewHero.onclick=() => {
     randomId = Math.floor(Math.random()*731) 
     getHero(randomId)
+}
+
+const displayName=()=>{
+nameOfHero.innerText= json.name
+    
 }
