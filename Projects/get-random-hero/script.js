22109f5c23f9a854281ec15f0d7bff60c0214c0b
haileyesus <teamdevs7@gmail.com>
2023-06-09 10:31:42 +0100

    final version

--- Projects/get-random-hero/script.js
+++ Projects/get-random-hero/script.js
@@ -1,6 +1,4 @@
-//261709416369407
-
-const BaseUrl = `https://superheroapi.com/api.php/261709416369407`
+ const BaseUrl = `https://superheroapi.com/api.php/261709416369407`
 const displayImg = document.getElementById('display')
 const getNewHero = document.getElementById('getNewHero')
 const inputField = document.getElementById('inputField')
@@ -12,35 +10,37 @@ const getHero = (id) =>{
     fetch(`${BaseUrl}/${id}`)
     .then(response => response.json())
     .then(json => {
-        console.log(json.powerstats)
-        statusOfHero.innerHTML = `<h3> Intelligence = ${json.powerstats['intelligence']}  </h3>`
+        const stats= getStats(json)
         nameOfHero.innerText= json.name
         let heroImg = json.image.url
-        displayImg.innerHTML =`<img src = "${json.image.url}" height = 250 width =200/>`
-
+        displayImg.innerHTML =`<img src = "${json.image.url}" height = 250 width =200/> ${stats}`
     })
 }
 
 const searchSuperHero =(name)=>{
-    //console.log(inputField.value)
     fetch(`${BaseUrl}/search/${name}`)
     .then(response=>response.json())
  .then(json=>{
     const hero =json.results[0]
-    //nameOfHero.innerText=`<h2>${json.name}</h2>`
-        displayImg.innerHTML =`<img src = "${hero.image.url}" height = 250 width =200/>`
+    const searchStats = getStats(hero)
+    displayImg.innerHTML =`<img src = "${hero.image.url}" height = 250 width =200/> ${searchStats}`
 
 })
 }  
 
 searchBtn.onclick=()=>searchSuperHero(inputField.value)
-//console.log(randomId)
 getNewHero.onclick=() => {
     randomId = Math.floor(Math.random()*731) 
     getHero(randomId)
 }
 
 const displayName=()=>{
-nameOfHero.innerText= json.name
-    
+nameOfHero.innerText= json.name   
+}
+
+const getStats=(character) =>{
+   const stat = Object.keys(character.powerstats).map(stat =>{
+        return `<p> ${stat.toUpperCase()}: ${character.powerstats[stat]}</p>`
+    })
+    return stat.join('')
 }
